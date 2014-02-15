671c6a776cb4e82d84dcd27b63b01873d3d1f3ab
Ivan Nečas <necasik@gmail.com>
2014-02-15 23:50:32 +0100

    Add possibility to trigger the commands via API

--- app/controllers/commands_controller.rb
+++ app/controllers/commands_controller.rb
@@ -9,16 +9,31 @@ class CommandsController < ApplicationController
   end
 
   def create
-    commands = params[:commands].split("\n").select(&:present?).map do |cmd|
-      { cmd: cmd }
+    commands = []
+    if params[:commands].is_a? String
+      commands = params[:commands].split("\n").select(&:present?).map do |cmd|
+        { cmd: cmd }
+      end
+    elsif params[:commands].is_a? Array
+      commands = params[:commands].map { |command| { cmd: command } }
     end
     if commands.any?
       task = Dyntask.async_task(Sysflow::Actions::BulkAction,
                                 Sysflow::Actions::Command,
                                 commands)
-      redirect_to(command_path(id: task.id))
+      respond_to do |format|
+        format.html { redirect_to(command_path(id: task.id)) }
+        format.json { render_task(task) }
+      end
+
     else
       redirect_to(commands_path, alert: "Specify at least one command")
     end
+  end
+
+  protected
+
+  def render_task(task)
+    render 'dyntask/api/tasks/show', :locals => { :task => task }
   end
 end
